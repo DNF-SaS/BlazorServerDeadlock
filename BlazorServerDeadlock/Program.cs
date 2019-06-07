e9f6e821f74193f2173e0b2cfc2ccce9db71f85d
sif <sif@dotnetfabrik.de>
2019-06-07 10:42:08 +0200

    Initial commit

---
+++ BlazorServerDeadlock/Program.cs
@@ -0,0 +1,30 @@
+﻿// <copyright file="Program.cs" company="DotNetFabrik">
+//     Copyright (c) DotNetFabrik － All rights reserved.
+// </copyright>
+
+namespace BlazorServerDeadlock
+{
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.Hosting;
+    using Microsoft.Extensions.Logging;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.ConfigureLogging(lb => lb
+                            .AddDebug()
+                            .AddConsole()
+                            .SetMinimumLevel(LogLevel.Trace));
+
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
